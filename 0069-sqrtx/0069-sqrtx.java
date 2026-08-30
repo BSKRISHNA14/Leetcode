2580436e1762e36058b6c1450cfb4146cab741be
BSKRISHNA14 <bskrishna1012@gmail.com>
2026-08-30 16:30:05 +0530

    Time: 1 ms (98.68%), Space: 42.3 MB (90.39%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int mySqrt(int x) {
+        long left=0;
+        long right=x/2;
+
+        if(x<2) return x;
+        while(left<=right){
+            long mid=left+(right-left)/2;
+            long square=mid*mid;
+
+            if(square==x){
+                return (int) mid;
+            }
+            else if(square<x){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return (int) right;
+    }
+}
