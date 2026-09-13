dcd83e4428b7723d4bf9c79f475a774315315684
BSKRISHNA14 <bskrishna1012@gmail.com>
2026-09-13 17:50:48 +0530

    Time: 6 ms (62.07%), Space: 42.7 MB (90.4%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -3,8 +3,8 @@ class Solution {
         int n=s.length();
         int count=0;
         for(int i=0;i<n;i++){
-            count+=expand(s,i,i);
-            count+=expand(s,i,i+1);
+            count+=expand(s,i,i);     //odd length 
+            count+=expand(s,i,i+1);   //even length
         }
         return count;
     }
