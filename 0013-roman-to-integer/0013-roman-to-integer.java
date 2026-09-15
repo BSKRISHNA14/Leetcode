fab41711a582fce617172a8c1c4b6a79d4f41277
BSKRISHNA14 <bskrishna1012@gmail.com>
2026-09-15 17:43:16 +0530

    Time: 4 ms (51.21%), Space: 46.7 MB (27.22%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -16,15 +16,14 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             int value = map.get(s.charAt(i));
 
-            // Check if a next character exists 
-            // AND check if current value is smaller than next value
+            // Check if a next character exists && // AND check if current value is smaller than next value
             if (i + 1 < s.length() && value < map.get(s.charAt(i + 1))) {
                 
                 result = result - value;   // If current value is smaller, subtract it
             
             } 
             else {
-                result = result + value;
+                result = result + value;   //
             }
         }
         return result;
