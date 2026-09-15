e5e80fee76be99961ef9d3edc733d89d26f78fab
BSKRISHNA14 <bskrishna1012@gmail.com>
2026-09-15 17:42:24 +0530

    Time: 4 ms (51.21%), Space: 46.7 MB (27.22%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,23 +1,31 @@
 class Solution {
     public int romanToInt(String s) {
-        HashMap<Character,Integer> map=new HashMap<>();
-        map.put('M',1000);
-        map.put('D',500);
-        map.put('C',100);
-        map.put('L',50);
-        map.put('X',10);
-        map.put('V',5);
-        map.put('I',1);
-        
-        int result=0;
-        for(int i=0;i<s.length();i++){
-           int value=map.get(s.charAt(i));
-           if(i+1<s.length() && value<map.get(s.charAt(i+1))){
-              result=result-value;
-           }
-           else{
-              result=result+value;
-           }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        map.put('M', 1000);
+        map.put('D', 500);
+        map.put('C', 100);
+        map.put('L', 50);
+        map.put('X', 10);
+        map.put('V', 5);
+        map.put('I', 1);
+
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int value = map.get(s.charAt(i));
+
+            // Check if a next character exists 
+            // AND check if current value is smaller than next value
+            if (i + 1 < s.length() && value < map.get(s.charAt(i + 1))) {
+                
+                result = result - value;   // If current value is smaller, subtract it
+            
+            } 
+            else {
+                result = result + value;
+            }
         }
         return result;
     }
