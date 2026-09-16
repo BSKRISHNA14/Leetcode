7b67c3e908ce31855118ed97c996abb144605450
BSKRISHNA14 <bskrishna1012@gmail.com>
2026-09-16 13:00:22 +0530

    Time: 1 ms (59.99%), Space: 43.3 MB (50.49%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -7,6 +7,6 @@ class Solution {
         while(i<first.length() && i<last.length() && first.charAt(i)==last.charAt(i)){
             i++;
         }
-        return last.substring(0,i);
+        return first.substring(0,i);
     }
 }
