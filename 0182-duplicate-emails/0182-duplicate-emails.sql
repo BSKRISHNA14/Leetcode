97bc0c314627d88149ced62a996524e0b2029be8
BSKRISHNA14 <bskrishna1012@gmail.com>
2026-09-18 00:09:16 +0530

    Time: 373 ms (88.68%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,1 +1,5 @@
-select email from person where email is not null group by email having count(email)>1;
+select email 
+from person 
+where email is not null 
+group by email 
+having count(email)>1;
