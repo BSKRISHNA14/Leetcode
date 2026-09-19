67ca2a36d2b8f65a58cbf0d6bbe211c5d05fec35
BSKRISHNA14 <bskrishna1012@gmail.com>
2026-09-19 23:34:48 +0530

    Time: 270 ms (90.81%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -4,3 +4,5 @@ SELECT (
     ORDER BY salary DESC
     LIMIT 1 OFFSET 1
 ) AS SecondHighestSalary;
+
+#SELECT MAX(salary) AS SecondHighestSalary FROM Employee WHERE salary < (SELECT MAX(salary) FROM Employee);
